c9edd1c27ec2ff6d0254b71661ab5e88f822ff22
Al-Amin hossain <252310005101005@stamford.university>
2025-05-26 21:01:09 +0600

    Refactor navigation structure by removing Primary and Secondary controllers, and introducing a new Transition controller for improved user flow. Also added text-field hover effect and global button hand cursor

--- src/main/java/com/bytebender/premnoybiye/transition.java
+++ src/main/java/com/bytebender/premnoybiye/transition.java
@@ -1,10 +1,14 @@
 package com.bytebender.premnoybiye;
 
 import java.io.IOException;
-
 import javafx.fxml.FXML;
 
 public class Transition {
+
+    @FXML
+    private void switchToTransition() throws IOException {
+        App.setRoot("transition");
+    }
 
     @FXML
     private void switchToLogin() throws IOException {
