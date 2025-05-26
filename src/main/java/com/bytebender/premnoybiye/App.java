4492e1d1352fd9aa701bfb4c3fa8560d2558cd1c
Al-Amin hossain <252310005101005@stamford.university>
2025-05-26 21:28:32 +0600

    optimized codebase

--- src/main/java/com/bytebender/premnoybiye/App.java
+++ src/main/java/com/bytebender/premnoybiye/App.java
@@ -6,39 +6,36 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+/*  JavaFX App */
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     private static Scene scene;
 
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+
+        scene.setRoot(loadFXML(fxml));
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
+
         scene = new Scene(loadFXML("primary"), 1000, 600);
+        stage.setScene(scene);
+        stage.setTitle("Prem Noy Biye");
+        stage.show();
 
         String css = this.getClass().getResource("application.css").toExternalForm();
         scene.getStylesheets().add(css);
 
-        stage.setScene(scene);
-        stage.setTitle("Prem Noy Biye");
-
         Image icon = new Image(App.class.getResourceAsStream("/com/bytebender/premnoybiye/img/Main-Logo.png"));
-
         stage.getIcons().add(icon);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
