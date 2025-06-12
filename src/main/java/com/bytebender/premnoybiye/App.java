19e8839c13b5282b4da4e93db5541d1ac74fc543
Al-Amin hossain <252310005101005@stamford.university>
2025-06-13 01:33:00 +0600

    Refactor FirebaseConnection and userInfo to use Firebase Authentication UID as primary key; adjust splash screen delay in App

--- src/main/java/com/bytebender/premnoybiye/App.java
+++ src/main/java/com/bytebender/premnoybiye/App.java
@@ -41,17 +41,17 @@ public class App extends Application {
         Image icon = new Image(App.class.getResourceAsStream("/com/bytebender/premnoybiye/img/Main-Logo.png"));
         stage.getIcons().add(icon);
 
-        // // Triggering delay to switch Splash Screen
-        // PauseTransition delay = new PauseTransition(Duration.seconds(1));
-        // // default timing 6second decided
-        // delay.play();
-        // delay.setOnFinished(event -> { // Code to execute after the delay
-        // try {
-        // App.setRoot("loginsignupchoice");
-        // } catch (IOException e) {
-        // e.printStackTrace();
-        // }
-        // });
+        // Triggering delay to switch Splash Screen
+        PauseTransition delay = new PauseTransition(Duration.seconds(0.5));
+        // default timing 6second decided
+        delay.play();
+        delay.setOnFinished(event -> { // Code to execute after the delay
+            try {
+                App.setRoot("loginsignupchoice");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     public static void main(String[] args) {
