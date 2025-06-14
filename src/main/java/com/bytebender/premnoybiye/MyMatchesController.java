67ba548a6fe4a55aa5056203323084dbd0c0523e
Al-Amin hossain <252310005101005@stamford.university>
2025-06-14 16:40:32 +0600

    feat: Add application icon to profile modals in Chat and MyMatches controllers

--- src/main/java/com/bytebender/premnoybiye/MyMatchesController.java
+++ src/main/java/com/bytebender/premnoybiye/MyMatchesController.java
@@ -16,6 +16,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -216,6 +217,9 @@ public class MyMatchesController {
             // Create a new stage for the modal
             Stage modalStage = new Stage();
             modalController.setModalStage(modalStage);
+
+            Image icon = new Image(App.class.getResourceAsStream("/com/bytebender/premnoybiye/img/Main-Logo.png"));
+            modalStage.getIcons().add(icon);
 
             modalStage.setTitle(user.getName() + "'s Profile");
             modalStage.initModality(Modality.APPLICATION_MODAL);
